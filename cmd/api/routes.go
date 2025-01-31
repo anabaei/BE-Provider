82abfdc6e255c461e3a8740d47ce45942f20ff54
anabaei <nabaei17@gmail.com>
2025-01-30 23:11:15 -0800

    define handlers for crud

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,7 +10,7 @@ import (
 func (app *application) routes() *http.ServeMux{
 	mux := http.NewServeMux()
 	mux.HandleFunc("/v1/healthcheck", app.healthcheck)
-	mux.HandleFunc("/v1/books", app.getCreateBooksHandler)
+	mux.HandleFunc("/v1/book/", app.getCreateBooksHandler)
 	mux.HandleFunc("/v1/books/", app.getUpdateDeleteBooksHandler)
     return mux 
 }
