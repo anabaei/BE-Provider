f4c1133db5c5db501241d3403fd21459fef45028
anabaei <nabaei17@gmail.com>
2025-02-01 16:35:58 -0800

    define json format

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,35 +1,48 @@
 package main
 
-
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	 "strconv"
+	"readinglist.duffney.io/internal/data"
+	"strconv"
+	"time"
 )
 
-func (app *application) healthcheck(w http.ResponseWriter, r *http.Request){
-	if(r.Method != http.MethodGet) {
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed) ,http.StatusMethodNotAllowed )
+func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		return
-	  }
-	  w.Header().Set("Content-Type", "text/plain")
-	  fmt.Fprintf(w, "API is up and running\nenvironment: %s\n", app.config.env)
+	}
+	data := map[string]string{
+		"status":      "ok",
+		"environment": app.config.env,
+		"version":     "1.0.0",
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	js = append(js, '\n')
+	w.Write(js)
+
 }
 
 func (app *application) getCreateBooksHandler(w http.ResponseWriter, r *http.Request) {
-	if(r.Method == http.MethodGet) {
+	if r.Method == http.MethodGet {
 		fmt.Fprintln(w, "Display a list of books on reading list")
 		return
-	  }
-	  if(r.Method == http.MethodPost) {
+	}
+	if r.Method == http.MethodPost {
 		fmt.Fprintln(w, "Post a new book to the list")
 		return
-	  }
+	}
 }
 
-
 func (app *application) getUpdateDeleteBooksHandler(w http.ResponseWriter, r *http.Request) {
-	
 	switch r.Method {
 	case http.MethodPut:
 		fmt.Fprintln(w, "update a book")
@@ -37,13 +50,16 @@ func (app *application) getUpdateDeleteBooksHandler(w http.ResponseWriter, r *ht
 	case http.MethodGet:
 		fmt.Fprintln(w, "GET a book")
 		app.getBook(w, r)
-	case http.MethodDelete: 
-	fmt.Fprintln(w, "Delete a book")
-	  app.deleteBook(w, r)
+	case http.MethodPost:
+		app.postBook(w, r)
+	case http.MethodDelete:
+		fmt.Fprintln(w, "Delete a book")
+		app.deleteBook(w, r)
 	default:
+		fmt.Fprintf(w, "came here")
+		app.updateBook(w, r)
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 	}
-
 
 }
 
@@ -51,27 +67,59 @@ func (app *application) getBook(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Path[len("/v1/books/"):]
 	idInt, err := strconv.ParseInt(string(id), 10, 64)
 	if err != nil {
-		http.Error(w, "Invalid book ID: " + err.Error(), http.StatusBadRequest)
+		http.Error(w, "Invalid book ID: "+err.Error(), http.StatusBadRequest)
 		return
 	}
-	fmt.Fprintf(w, "GET book with ID %d\n", idInt)
+
+	book := data.Book{
+
+		ID:        idInt,
+		Title:     "The Great Gatsby",
+		CreatedAt: time.Now(),
+		Published: time.Date(2015, 4, 10, 0, 0, 0, 0, time.UTC),
+		Pages:     218,
+		Genres:    []string{"Fiction", "Tragedy"},
+		Rating:    4.5,
+		Version:   1,
+	}
+
+	js, err := json.Marshal(book)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+	return
 }
 
 func (app *application) updateBook(w http.ResponseWriter, r *http.Request) {
+
 	id := r.URL.Path[len("/v1/books/"):]
 	idInt, err := strconv.ParseInt(string(id), 10, 64)
 	if err != nil {
-		http.Error(w, "Invalid book ID: " + err.Error(), http.StatusBadRequest)
+		http.Error(w, "Invalid book ID: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 	fmt.Fprintf(w, "update book with ID %d\n", idInt)
+}
+
+func (app *application) postBook(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/v1/books/"):]
+	idInt, err := strconv.ParseInt(string(id), 10, 64)
+	if err != nil {
+		http.Error(w, "Invalid book ID: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "post book with ID %d\n", idInt)
 }
 
 func (app *application) deleteBook(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Path[len("/v1/books/"):]
 	idInt, err := strconv.ParseInt(string(id), 10, 64)
 	if err != nil {
-		http.Error(w, "Invalid book ID: " + err.Error(), http.StatusBadRequest)
+		http.Error(w, "Invalid book ID: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 	fmt.Fprintf(w, "delete book with ID %d\n", idInt)
