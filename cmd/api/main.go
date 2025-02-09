798af48c79ca6c37b9bbd0550f5f6e5819ac447f
anabaei <nabaei17@gmail.com>
2025-02-09 12:29:53 -0800

    Add PostgreSQL database connection and models for book management

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,28 +8,27 @@ import (
 	"time"
 	"os"
 	"database/sql"
-	"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
+
+const version = "1.0.0"
 
 type config struct {
 	port int
 	env string
+	dsn string //data name service or connection string to connect to database
 }
-
 
 type application struct {
 	config config
 	logger *log.Logger
 }
 
-
 func main() {
-
 	var cfg config
-
 	flag.IntVar(&cfg.port, "port", 4001, "API server port")
 	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev, test, prod)")
-    flag.StringVar(&cfg.dsn, os.GetEnv("Readme"))
+    flag.StringVar(&cfg.dsn, "db-dsn", os.Getenv("READINGLIST_DB_DSN"), "PostgreSQL DSN")
 	flag.Parse()
 
     logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
@@ -37,7 +36,25 @@ func main() {
 		config: cfg, 
 		logger: logger,
 	}
+   // dsn := "user=postgres password=password dbname=readinglist sslmode=disable"
+    db, err := sql.Open("postgres", cfg.dsn)
+	//db, err := sql.Open("postgres", dsn)
+	
+	if err != nil {
+		logger.Fatal(err)
+	}
 
+    defer db.Close()
+    // test connection
+
+    err = db.Ping()
+    if err != nil {
+		logger.Fatal(err)
+	}
+    
+	logger.Printf("database connection pool established")
+
+    
 	// mux := http.NewServeMux()
 
 	// Add router debug logging
@@ -54,8 +71,8 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	err := srv.ListenAndServe()
-	logger.Fatal(err)
+	errr := srv.ListenAndServe()
+	logger.Fatal(errr)
 }
 
 
--- internal/data/books.go
+++ internal/data/books.go
@@ -1,7 +1,9 @@
 package data
 
 import (
+    "database/sql"
 	"time"
+    "github.com/lib/pq"
 )
 
 type Book struct {
@@ -14,3 +16,17 @@ type Book struct {
     Rating    float64   `json:"rating"`
     Version   int       `json:"version"`
 }
+
+type BookModel struct{
+    DB *sql.DB
+}
+
+
+func (b BookModel) Insert(book *book) error {
+    query := `INSERT INTO books (title, published, pages
+    , genres, rating) VALUES($1, $2, $3, $4, $5) 
+    RETURNING id, created_at, version`
+
+    args := []interface{}(book.Title, book.Published, book.Pages, pq.Array(book.Genres), book.rating)
+    return b.DB.queryRow(query, args...).Scan(&book.ID, &book.CreatedAt, &book.Version)
+} 
